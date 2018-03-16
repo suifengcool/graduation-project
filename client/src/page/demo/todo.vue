65a3971141bdff1114352fbe373a177a70e149a9
xugang <371801080@qq.com>
2018-03-16 15:07:00 +0800

    todo

--- client/src/page/demo/todo.vue
+++ client/src/page/demo/todo.vue
@@ -1,26 +1,34 @@
 <template>
-    <div class="demo-todo">
-        Todo
-    </div>
+  <div id="app">
+    <h1>Vue Vuex Todo Demo</h1>
+    <hr>
+    <todoInput />
+    <todoList />
+    <p>todo数量：{{todoCount}}</p>
+  </div>
 </template>
 
 <script>
-
-export default {
-    name: 'Todo',
-
+  import todoInput from '../../components/input.vue';
+  import todoList from '../../components/list.vue';
+  import {mapGetters} from "vuex";
+  export default {
+    name: 'app',
     data () {
-        return {
-
-        }
+      return {
+      }
     },
-
-    methods: {
-        
+    computed:{
+      ...mapGetters({
+        todoCount:"todoCount"      //映射Getter
+      })
+    },
+    components:{
+      todoInput,
+      todoList
     }
-}
+  }
 </script>
 
-<style scoped lang="sass">
-
+<style>
 </style>
