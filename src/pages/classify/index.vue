3f00d71a5c232258d79047a8f1094d2914edbfd8
zhoukang <823345482@qq.com>
2018-05-07 21:50:47 +0800

    分类设置

--- src/pages/classify/index.vue
+++ src/pages/classify/index.vue
@@ -8,24 +8,23 @@
       <div class="title">
         <Row type="flex" align="middle" justify="center">
           <Col :span="5"><div class="titleInfo">分类名称</div></Col>
-          <Col :span="5"><div class="titleInfo">分类权重</div></Col>
-          <!-- <Col :span="4"><div class="titleInfo">分类图标</div></Col> -->
           <Col :span="5"><div class="titleInfo">创建时间</div></Col>
+          <!-- <Col :span="4"><div class="titleInfo">分类图标</div></Col> -->
+          <Col :span="5"><div class="titleInfo">更新时间</div></Col>
           <Col :span="5"><div class="titleInfo">操作</div></Col>
         </Row>
       </div>
       <!-- 主体部分 -->
       <div class="main" id="main">
-        <Collapse accordion>
-          <CollapseItem v-for="(item,index) in options" :key="index">
+        <Collapse accordion @change="changeStatus">
+          <CollapseItem v-for="(item,index) in options" :key="index" :name="item.id">
             <template slot="title">
               <Row type="flex" align="middle" justify="center">
                 <Col :span="5"><Input v-model.trim="item.name"  placeholder="请输入主分类名称" style="width:200px"></Input></Col>
-                <Col :span="5"><Input type="number" v-model.number="item.weight"  placeholder="请输入权重" style="width:110px"></Input></Col>
-                <!-- <Col :span="5" style="display: flex;justify-content: center;">
-                  <Avaters :url="item.icon" @success="item.icon = $event" :width="'40'" :height="'40'"/>
-                </Col> -->
-                <Col :span="5"><div>{{detailTimeFilter(0,0,item.createTime)}}</div></Col>
+                <Col :span="5">
+                  <div>{{detailTimeFilter(0,0,item.createTime)}}</div>
+                </Col>
+                <Col :span="5"><div>{{detailTimeFilter(0,0,item.updateTime)}}</div></Col>
                 <Col :span="5">
                   <div>
                     <Button type="danger" plain size="middle" @click.stop.native="saveDad(item)">保存</Button>
@@ -34,18 +33,20 @@
                 </Col>
               </Row>
             </template>
-            <div v-for="(ite,index) in item.list" class="sonClassify" :key="index">
+            <div v-for="(ite,index) in optionss" class="sonClassify" :key="index">
               <Row type="flex" align="middle" justify="center">
                 <Col :span="5"><Input v-model.trim="ite.name"  placeholder="请输入子分类名称" style="width:150px"></Input></Col>
-                <Col :span="5"><Input type="number" v-model.number="ite.weight" placeholder="请输入权重" style="width:110px"></Input></Col>
+                <Col :span="5">
+                  <div>{{detailTimeFilter(0,0,ite.createTime)}}</div>
+                </Col>
                 <!-- <Col :span="5" style="display: flex;justify-content: center;">
                   <Avaters :url="ite.icon" @success="ite.icon = $event" :width="'40'" :height="'40'"/>
                 </Col> -->
-                <Col :span="5"><div>{{detailTimeFilter(0,0,ite.createTime)}}</div></Col>
+                <Col :span="5"><div>{{detailTimeFilter(0,0,ite.updateTime)}}</div></Col>
                 <Col :span="5">
                   <div>
                     <Button type="danger" plain size="middle"  @click="saveSon(ite,item.id)">保存</Button>
-                    <Button type="danger" plain size="middle" @click="delSon(ite)">删除</Button>
+                    <Button type="danger" plain size="middle" @click="delSon(item,ite)">删除</Button>
                   </div>
                 </Col>
               </Row>
@@ -80,6 +81,7 @@ export default {
 		return {
        data: [],
         options:[],
+        optionss:[],
         aaa:0,
         showData: [
           {
@@ -102,120 +104,110 @@ export default {
         console.log(val)
       },
       init(){
-      //   vm.fetch.post("/mechanism/attachedInformationList/0").then(res => {
-      //  this.data.list= res.data
-      //  this.loading = false
-      // //  console.log(res.data)
-      //  // 增加weight片段
-      //   this.weights =  this.data.list.map((item,index)=>{
-      //     return item.weight
-      //   })       
-      // })
         vm.fetch.get("/classify/list?page=1&pageSize=10").then(res => {
-          cosole.log(res);
+          this.options=res.list;
+          console.log(res);
         })
       },
+      changeStatus(res){
+        if(res){
+          vm.fetch.get(`/classify/findchildren/${res}`).then(res => {
+          this.optionss=res;
+          console.log(res,"xxx");
+        })
+        }
+      },
       saveSon(res,resID){
-        // var newAddDad={}
-        // if(res.id){
-        //   console.log("修改")
-        //   newAddDad.parentId=resID
-        //   newAddDad.name = res.name
-        //   newAddDad.icon = res.icon
-        //   newAddDad.weight=res.weight
-        //   updategoodsCategory(res.id,newAddDad).then((data)=>{ //新增商品分类
-        //     if(data.code==812||data.code==813){
-        //       return;
-        //     }
-        //     this.init()
-        //     this.$message({
-        //       type: 'success',
-        //       message: '修改成功!'
-        //     });
-        //     newAddDad={}
-        //     // console.log("zzzz",newAddDad)
-        //   })
-        // }else{
-        //   console.log("新增")
-        //   newAddDad.parentId=resID
-        //   newAddDad.name = res.name
-        //   newAddDad.icon = res.icon
-        //   newAddDad.weight=res.weight
-        //   // console.log("333333",newAddDad)
-        //   addgoodsCategory(newAddDad).then((data)=>{ //新增商品分类
-        //     if(data.code==812||data.code==813||data.code==814){
-        //       return;
-        //     }
-        //     this.init()
-        //     this.$message({
-        //       type: 'success',
-        //       message: '新建成功!'
-        //     });
-        //     newAddDad={}
-        //     // console.log("zzzz",newAddDad)
-        //   })
-        // }
+        var newAddDad={}
+        if(res.id){
+          // console.log("修改")
+          // newAddDad.parentId=resID
+          // newAddDad.name = res.name
+          // newAddDad.icon = res.icon
+          // newAddDad.weight=res.weight
+          // updategoodsCategory(res.id,newAddDad).then((data)=>{ //新增商品分类
+          //   if(data.code==812||data.code==813){
+          //     return;
+          //   }
+          //   this.init()
+          //   this.$message({
+          //     type: 'success',
+          //     message: '修改成功!'
+          //   });
+          //   newAddDad={}
+          //   // console.log("zzzz",newAddDad)
+          // })
+        }else{
+          console.log("新增")
+          newAddDad.parentId=resID
+          newAddDad.name = res.name
+          // console.log("333333",newAddDad)
+          vm.fetch.post("/classify/add",newAddDad).then(data => {
+            this.$message({
+              type: 'success',
+              message: '新建成功!'
+            });
+            vm.fetch.get(`/classify/findchildren/${resID}`).then(res => {
+              this.optionss=res;
+              // console.log(res,"xxx");
+           })
+            newAddDad={}
+          })
+        }
       },
       saveDad(res){
-        // var newAddDad={}
-        // // stop
-        // if(res.id){
-        //   console.log("修改")
-        //   this.options.forEach((element,key) => {
-        //     if(element.id==res.id){
-        //       newAddDad.parentId=0
-        //       newAddDad.name = this.options[key].name
-        //       newAddDad.icon = this.options[key].icon
-        //       newAddDad.weight=this.options[key].weight
-        //     }
-        //   })
-        //   updategoodsCategory(res.id,newAddDad).then((data)=>{ 
-        //   if(data.code==812||data.code==813){
-        //       return;
-        //     }
-        //     this.init()
-        //     this.$message({
-        //       type: 'success',
-        //       message: '修改成功!'
-        //     });
-        //     newAddDad={}
-        //     // console.log("zzzz",newAddDad)
-        //   })
-        // }else{
+        var newAddDad={}
+        // stop
+        if(res.id){
+          console.log("修改")
+          this.options.forEach((element,key) => {
+            if(element.id==res.id){
+              // newAddDad.parentId=0
+              // newAddDad.name = this.options[key].name
+              // newAddDad.icon = this.options[key].icon
+              // newAddDad.weight=this.options[key].weight
+            }
+          })
+          // updategoodsCategory(res.id,newAddDad).then((data)=>{ 
+          // if(data.code==812||data.code==813){
+          //     return;
+          //   }
+          //   this.init()
+          //   this.$message({
+          //     type: 'success',
+          //     message: '修改成功!'
+          //   });
+          //   newAddDad={}
+          //   // console.log("zzzz",newAddDad)
+          // })
+        }else{
 
-        //   this.options.forEach((element,key) => {
-        //     if(!element.id){
-        //       newAddDad.parentId=0
-        //       newAddDad.name = this.options[key].name
-        //       newAddDad.icon = this.options[key].icon
-        //       newAddDad.weight=this.options[key].weight
-        //     }
-        //   })
-        //   // console.log("333333",newAddDad)
-        //   addgoodsCategory(newAddDad).then((data)=>{ //新增商品分类
-        //     if(data.code==812||data.code==813||data.code==814){
-        //       return;
-        //     }
-        //     this.init()
-        //     this.$message({
-        //       type: 'success',
-        //       message: '新建成功!'
-        //     });
-        //     newAddDad={}
-        //     // console.log("zzzz",newAddDad)
-        //   })
-        //   console.log("新增")
-        // }
+          this.options.forEach((element,key) => {
+            if(!element.id){
+              // newAddDad.parentId=0
+              newAddDad.name = this.options[key].name
+            }
+          })
+          // console.log("333333",newAddDad)
+          // addgoodsCategory(newAddDad).then((data)=>{ 
+          vm.fetch.post("/classify/add",newAddDad).then(data => {
+            this.init()
+            this.$message({
+              type: 'success',
+              message: '新建成功!'
+            });
+            newAddDad={}
+
+          })
+          console.log("新增")
+        }
       },
       addDad(){
         let newDad1={
           parentId:0,
           name:'',
-          weight:'',
-          list:[]
         }
         this.options.unshift(newDad1)
-
         console.log(this.options,"asfsdf")
       },
       addSon(res){//子分类
@@ -223,32 +215,32 @@ export default {
         let newSon1={
           parentId:res.id,
           name:'',
-          weight:''
         }
         this.options.forEach((element,key) => {
           if(element.id==res.id){
-            this.options[key].list.unshift(newSon1)
+            this.optionss.unshift(newSon1)
             console.log(this.options,'zifen')
           }
         })
       },
-      delSon(res){
+      delSon(item,ite){
         MessageBox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
           type: 'warning',
           center: true
         }).then(() => {
-          // delgoodsCategory(res.id).then((data)=>{ //获取商品分类
-          //  if(data.code==815||data.code==811){
-          //     return;
-          //   }
-          //   this.init()
-             vm.$message({
+          vm.fetch.delete(`/classify/delete/${ite.id}`).then(data => {
+           vm.$message({
               type: 'success',
-              message: '删除成功!'
+              message: '删除成功!'   
             });
-          // });
+           vm.fetch.get(`/classify/findchildren/${item.id}`).then(res => {
+              this.optionss=res;
+              // console.log(res,"xxx");
+           })
+            // this.init()
+        })
         }).catch(() => {
           vm.$message({
             type: 'info',
@@ -263,15 +255,13 @@ export default {
           type: 'warning',
           center: true
         }).then(() => {
-          // delgoodsCategory(res.id).then((data)=>{ 
-          //   if(data.code==815||data.code==811){
-          //     return;
-          //   }
-          //   this.init()
-            vm.$message({
+        vm.fetch.delete(`/classify/delete/${res.id}`).then(data => {
+           vm.$message({
               type: 'success',
               message: '删除成功!'
             });
+            this.init()
+        })
           // })
 
         }).catch(() => {
