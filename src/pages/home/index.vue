5d75d7140816fcddb9e8dae46321866e95bfa08c
yuanxiaohu <125733004@qq.com>
2018-05-07 21:52:06 +0800

    完成首页

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -3,7 +3,7 @@
         <HomeCarousel :data="carousel.list"></HomeCarousel>
         <div class="layout">
             <Row :gutter="76">
-              <Col :span="12">
+              <Col :span="12" v-for="(item,index) in data.list" :key="index">
               <!-- <router-link to='/'> -->
               <div class="role_pic">
                   <div class="solution solve">
@@ -13,7 +13,7 @@
                             <span class="line">
                             </span>
                             <span class="title_name">
-                              公告信息
+                              {{item.name || '暂无分类'}}
                             </span>
                             <span class="line">
                             </span>
@@ -22,7 +22,7 @@
                             </div>
                           </div>
                         </header>
-                          <Row :gutter="80">
+                          <Row :gutter="40">
                             <Col :span="12">
                               <div class="solve_img">
                                 <img :src="'/static/img/solve0.png'" alt="">
@@ -30,24 +30,21 @@
                               </Col>
                               <Col :span="12" >
                               <div class="solution_detail">
-                                <div class="detail">
-                                  <!-- {{data.detail}} -->方案描述方案描述方案描述方案描述方案描述方案描述方案描述方案描述
-                                </div>
-                                <h4>
-                                  方案描述
-                                </h4>
-                                <p>
-                                 <!-- {{data.descript}} -->方案描述方案描述方案描述方案描述方案描述
-                                </p>
+                                <ul class="detail">
+                                  <li v-for="(ele,idx) in item.list">
+                                    <router-link :to = "`/home/detail/${idx}`">
+                                      {{item.content}}
+                                    </router-link>
+                                  </li>
+                                </ul>
                               </div>
                             </Col>
                           </Row> 
                       </div>
                     </div>
               </div>
-              <!-- </router-link> -->
               </Col>
-              <Col :span="12">
+              <!-- <Col :span="12">
              
                 <div class="role_pic">
                     <div class="solution solve">
@@ -57,7 +54,7 @@
                               <span class="line">
                               </span>
                               <span class="title_name">
-                                本科教务
+                                {{classifyName[1]}}
                               </span>
                               <span class="line">
                               </span>
@@ -74,16 +71,12 @@
                                 </Col>
                                 <Col :span="12" >
                                 <div class="solution_detail">
-                                  <div class="detail">
-                                    <!-- {{data.detail}} -->方案描述方案描述方案描述方案描述方案描述方案描述方案描述方案描述
-                                  </div>
-                                  <h4>
-                                    方案描述
-                                  </h4>
-                                  <p>
-                                   <!-- {{data.descript}} -->方案描述方案描述方案描述方案描述方案描述
-                                  </p>
-                                </div>
+                                <ul class="detail">
+                                  <li v-for="(item,index) in announcement.list">
+                                    {{item.text}}
+                                  </li>
+                                </ul>
+                              </div>
                               </Col>
                             </Row> 
                         </div>
@@ -91,8 +84,8 @@
               </div>
               
     
-              </Col>
-              <Col :span="12">
+              </Col> -->
+              <!-- <Col :span="12">
               <div class="role_pic">
                   <div class="solution solve">
                       <div class="layout">
@@ -110,7 +103,7 @@
                             </div>
                           </div>
                         </header>
-                          <Row :gutter="80">
+                          <Row :gutter="40">
                             <Col :span="12">
                               <div class="solve_img">
                                 <img :src="'/static/img/solve2.png'" alt="">
@@ -118,23 +111,19 @@
                               </Col>
                               <Col :span="12" >
                               <div class="solution_detail">
-                                <div class="detail">
-                                  <!-- {{data.detail}} -->方案描述方案描述方案描述方案描述方案描述方案描述方案描述方案描述
-                                </div>
-                                <h4>
-                                  方案描述
-                                </h4>
-                                <p>
-                                 <!-- {{data.descript}} -->方案描述方案描述方案描述方案描述方案描述
-                                </p>
+                                <ul class="detail">
+                                  <li v-for="(item,index) in announcement.list">
+                                    {{item.text}}
+                                  </li>
+                                </ul>
                               </div>
                             </Col>
                           </Row> 
                       </div>
                     </div>
               </div>
-              </Col>
-              <Col :span="12">
+              </Col> -->
+              <!-- <Col :span="12">
              
                 <div class="role_pic">
                     <div class="solution solve">
@@ -160,68 +149,131 @@
                                 </div>
                                 </Col>
                                 <Col :span="12" >
-                                <div class="solution_detail">
-                                  <div class="detail">
-                                    <!-- {{data.detail}} -->方案描述方案描述方案描述方案描述方案描述方案描述方案描述方案描述
-                                  </div>
-                                  <h4>
-                                    方案描述
-                                  </h4>
-                                  <p>
-                                   <!-- {{data.descript}} -->方案描述方案描述方案描述方案描述方案描述
-                                  </p>
-                                </div>
+                               <div class="solution_detail">
+                                <ul class="detail">
+                                  <li v-for="(item,index) in announcement.list">
+                                    <router-link v-bind="{to:'/home/detail/'+index}">
+                                      {{item.text}}
+                                    </router-link>
+                                  </li>
+                                </ul>
+                              </div>
                               </Col>
                             </Row> 
                         </div>
                       </div>
               </div>
-              </Col>
+              </Col> -->
             </Row>
           </div>
     </div>
 </template>
 
 <script>
-import { Button,Row,Col} from 'element-ui'
+import { Button, Row, Col } from 'element-ui'
 import HomeCarousel from '../../components/HomeCarousel.vue'
 export default {
-    name: 'homeIndex',
-    components: {
-      Button,
-      HomeCarousel,
-      Row,
-      Col
-    },
-    data () {
-		return {
-      carousel:{
-        list:[
-        {imgUrl:'/static/img/banner01.jpg'},
-        {imgUrl:'/static/img/banner02.jpg'},
-        {imgUrl:'/static/img/banner03.jpg'},
-        {imgUrl:'/static/img/banner04.jpg'},
-
-        ],
+  name: 'homeIndex',
+  components: {
+    Button,
+    HomeCarousel,
+    Row,
+    Col
+  },
+  data() {
+    return {
+      data: {
+        list: []
       },
-		}
-    },
+      carousel: {
+        list: [
+          { path: '/static/img/banner01.jpg' },
+          { path: '/static/img/banner02.jpg' },
+          { path: '/static/img/banner03.jpg' },
+          { path: '/static/img/banner04.jpg' }
+        ]
+      },
+      announcement: {
+        list: [
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          },
+          {
+            text:
+              '关于计算机与信息学院计划招生2018 最深刻的还款记录的撒谎的撒娇卡萨电话卡三等奖哦啊。。。。'
+          }
+        ]
+      }
+    }
+  },
 
-    created () {
-    },
-
-    methods: {
-
-	}
+  created() {
+    //获取轮播图
+    // http://192.168.1.247:8089/pictures/list
+    vm.fetch.get('pictures/list').then(res => {
+      this.carousel.list = res.list
+    })
+    // 获取分类接口
+    vm.fetch.get('/classify/findchildren/2').then(res => {
+      this.data.list = res
+      console.log('分类---》', res)
+      let arr = []
+      res.forEach((item, index) => {
+        let obj = {}
+        obj.id = item.id
+        obj.name = item.name
+        vm.fetch.get('/articles/list?type=' + item.id).then(res => {
+          obj.list = res.list
+        })
+        arr.push(obj)
+      })
+      console.log('arr:', arr)
+      this.data.list = arr
+    })
+  },
+  methods: {}
 }
 </script>
 <style lang='less' scoped>
-  .home-index{
-    >.layout{
-      margin: 0 auto;
-      max-width: 1366px;
-    }
-    header {
+.home-index {
+  > .layout {
+    margin: 0 auto;
+    max-width: 1366px;
+  }
+  header {
     height: 160px;
     text-align: center; // line-height: 130px;
     font-family: MicrosoftYaHei;
@@ -278,34 +330,52 @@ export default {
       right: -19px;
     }
   }
-    /* margin-top:80px; */
-    .solution.solve {
+  /* margin-top:80px; */
+  .solution.solve {
     background-color: #fff;
     padding-bottom: 70px;
-    .layout{
-      .el-row{
-        .el-col-12{
+    .layout {
+      .el-row {
+        .el-col-12 {
           overflow: hidden;
         }
       }
     }
     .solve_img {
-      height: 250px;
+      height: 240px;
       img {
         height: 100%;
       }
     }
     .solution_detail {
-      padding-top: 33px;
       .detail {
         font-family: MicrosoftYaHei;
         font-size: 16px;
         font-weight: normal;
         font-stretch: normal;
-        line-height: 28px;
+        line-height: 24px;
         letter-spacing: 0px;
         color: #333333;
-        margin-bottom: 40px;
+        // margin-bottom: 40px;
+        li {
+          text-indent: 2em;
+          font-size: 14px;
+          position: relative;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+          &:before {
+            content: '';
+            display: inline-block;
+            height: 5px;
+            width: 5px;
+            position: absolute;
+            left: 10px;
+            top: 10px;
+            background-color: #51afc9;
+            border-radius: 50%;
+          }
+        }
       }
       h4 {
         font-family: MicrosoftYaHei;
@@ -323,5 +393,5 @@ export default {
       }
     }
   }
-  }
+}
 </style>
